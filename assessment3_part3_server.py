09d4d427b0eaeaef736c8cd7a9458090cda32864
Rezalini <166637558+Rezalini@users.noreply.github.com>
2024-11-04 15:14:21 +0800

    Update assessment3_part3_server.py

--- assessment3_part3_server.py
+++ assessment3_part3_server.py
@@ -5,24 +5,30 @@ Created on Mon Oct 21 12:38:49 2024
 @author: rezaf
 """
 
+# Import the socket library
 import socket
 
+#create varuble for socket
 s = socket.socket()
 
 print("Socket created")
 
+#specify the server name and port
 s.bind(("localhost",1234))
 
+# open the ports for connection
 s.listen(3)
 print("Waiting for connections")
 
+#accept the clients request
 c, addr, = s.accept()
 c.send(bytes("WLC to reza server","utf-8"))      
 
+#recieve the client massege
 name = c.recv(1024).decode()
 print("connected with ",addr,name) 
 
-    
+#terminate the program    
 c.close() 
 s.close()
     
